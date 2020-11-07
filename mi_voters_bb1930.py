dc3aa2e11616b8faa55f22568bc796a6050f651a
luke-slytalker <73183410+luke-slytalker@users.noreply.github.com>
2020-11-06 18:13:44 -0800

    Update and rename mi_voters.py to mi_voters_bb1930.py

--- mi_voters_bb1930.py
+++ mi_voters_bb1930.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+# grabs MI voters & filter for ones born before 1930
 url = "https://voterrecords.com/voters/mi/before+1930/"     # base search URL
 headers = {'User-Agent': 'Mozilla/5.0'}                     # headers so it doesn't think we're scraping
 
